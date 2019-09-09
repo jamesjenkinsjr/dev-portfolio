ed13a8c35f3e02ede85a93b1f0b4146a57cdc754
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-09 18:22:44 -0400

    get scroll working again

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ import Nav from './Nav'
 import Home from './Home'
 import Contact from './Contact'
 import Projects from './Projects'
+import $ from 'jquery'
+
+const handleScrollBottom = function() {
+  $(window).on('scroll', () => {
+    let height = $(window).height()
+    let scroll = $(window).scrollTop()
+    if (scroll > height) {
+      $('.scroll-top').removeClass('hidden')
+    } else {
+      $('.scroll-top').addClass('hidden')
+    }
+  })
+}
+
+const handleScrollClick = function() {
+  $('body').on('click', '.scroll-top', () => {
+    $('html').animate({ scrollTop: 0 }, 'slow')
+  })
+}
+
+const handlers = function() {
+  handleScrollBottom()
+  handleScrollClick()
+}
+
+$(handlers)
 
 class App extends React.Component {
   constructor() {
@@ -27,7 +53,6 @@ class App extends React.Component {
         {this.state.location === 'home' ? <Home /> : null}
         {this.state.location === 'contact' ? <Contact /> : null}
         {this.state.location === 'projects' ? <Projects /> : null}
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
       </div>
     )
   }
