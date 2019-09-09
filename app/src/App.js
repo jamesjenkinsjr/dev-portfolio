a84463bb22eef71f7dac393d2b9d60b6bb32589b
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-09 17:03:52 -0400

    get Projects component working

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import './App.css'
-import Nav from './Nav';
-import Home from './Home';
-import Contact from './Contact';
+import Nav from './Nav'
+import Home from './Home'
+import Contact from './Contact'
+import Projects from './Projects'
 
 function App() {
   const state = {
-    location: 'contact'
+    location: 'projects',
   }
+
+  // const handleNavigation
   return (
     <div className="App">
-      <body>
-
-        {state.location === 'home' ? (<Home />) : null}
-        {state.location === 'contact' ? (<Contact />) : null}
-        <Nav />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-      </body>
+      <Nav />
+      {state.location === 'home' ? <Home /> : null}
+      {state.location === 'contact' ? <Contact /> : null}
+      {state.location === 'projects' ? <Projects /> : null}
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     </div>
   )
 }
