d8d07713bd8ae05c445391e2ab75c4e90dca615f
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-09 16:53:27 -0400

    get Contact module working

--- app/src/App.js
+++ app/src/App.js
@@ -2,16 +2,18 @@ import React from 'react'
 import './App.css'
 import Nav from './Nav';
 import Home from './Home';
+import Contact from './Contact';
 
 function App() {
   const state = {
-    location: 'home'
+    location: 'contact'
   }
   return (
     <div className="App">
       <body>
 
         {state.location === 'home' ? (<Home />) : null}
+        {state.location === 'contact' ? (<Contact />) : null}
         <Nav />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
       </body>
