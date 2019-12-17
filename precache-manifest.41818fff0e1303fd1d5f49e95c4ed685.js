7de439114230045292e654ea74a53d683060e830
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-12-17 15:16:00 -0500

    Updates

---
+++ precache-manifest.41818fff0e1303fd1d5f49e95c4ed685.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5e8cfee05ebc6c6d918a558a78218e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f833fd03d4f9fa5edd4e",
+    "url": "/static/css/main.60634802.chunk.css"
+  },
+  {
+    "revision": "42eaeb89a025b0af5f71",
+    "url": "/static/js/2.5ccff6e2.chunk.js"
+  },
+  {
+    "revision": "f833fd03d4f9fa5edd4e",
+    "url": "/static/js/main.a965878f.chunk.js"
+  },
+  {
+    "revision": "f4470a1d358b84166157",
+    "url": "/static/js/runtime~main.edc4bf8c.js"
+  },
+  {
+    "revision": "889b05ab5d64911fadbc56addaddacc8",
+    "url": "/static/media/Anonygram-desktop.889b05ab.png"
+  },
+  {
+    "revision": "ba37fdc49d96463413bda4b5ec492f7e",
+    "url": "/static/media/Huddle-portal-desktop.ba37fdc4.png"
+  },
+  {
+    "revision": "31c66d1fe43fb9bd367fc0b85ccef62f",
+    "url": "/static/media/hero-photo.31c66d1f.png"
+  },
+  {
+    "revision": "d90950c4691b0f5c1ad45c27dadea2b7",
+    "url": "/static/media/syntrack-desktop.d90950c4.png"
+  }
+]);
--- to-do.md
+++
@@ -1,9 +0,0 @@
-- BUG: UX glitchy for back to top on mobile (fix or remove)
-
-
-CSS ideas?
-- hover states for clickable content
-
-JS ideas?
-- Back to top button to scroll mobile users
-- overlay on images in projects with icon links
