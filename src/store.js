26eccde221e95a3da732facd8598dd476fd2ac4b
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-12-17 15:03:15 -0500

    include latest and greatest projects

--- src/store.js
+++ src/store.js
@@ -1,7 +1,25 @@
-import Bookmarker from './assets/Bookmarker-preview.png'
+import Anonygram from './assets/Anonygram-desktop.png'
 import Huddle from './assets/Huddle-portal-desktop.png'
+import Syntrack from './assets/syntrack-desktop.png'
 
 const store = [
+  {
+    title: 'Anonygram',
+    description: 'Share and engage with location-based submissions in a social media application',
+    srcURL: 'https://github.com/jamesjenkinsjr/anonygram',
+    liveURL: 'https://anonygram.jamesjenkinsjr.now.sh/',
+    img: Anonygram,
+    techIcons: [
+      'devicon-html5-plain',
+      'devicon-css3-plain-wordmark',
+      "devicon-nodejs-plain",
+      "devicon-express-original",
+      "devicon-react-original",
+      "devicon-postgresql-plain",
+      "devicon-amazonwebservices-original",
+      "devicon-google-plain"
+    ]
+  },
   {
     title: 'Huddle',
     description: 'Instantly create fast, private message portals without providing personal information',
@@ -13,19 +31,23 @@ const store = [
       'devicon-css3-plain-wordmark',
       "devicon-nodejs-plain",
       "devicon-express-original",
-      "devicon-react-original"
+      "devicon-react-original",
+      "devicon-postgresql-plain"
     ]
   },
   {
-    title: 'Bookmarker',
-    description: 'Create and maintain bookmarks for favorite websites',
-    srcURL: 'https://github.com/jamesjenkinsjr/JamesJ-bookmarks-app',
-    liveURL: 'https://jamesjenkinsjr.github.io/JamesJ-bookmarks-app/',
-    img: Bookmarker,
+    title: 'Syntrack',
+    description: 'Learn Russian survival words and phrases using spaced repetition',
+    srcURL: 'https://github.com/jamesjenkinsjr/syntrack',
+    liveURL: 'https://syntrack-client.jamesjenkinsjr.now.sh/register',
+    img: Syntrack,
     techIcons: [
       'devicon-html5-plain',
       'devicon-css3-plain-wordmark',
-      'devicon-jquery-plain'
+      "devicon-nodejs-plain",
+      "devicon-express-original",
+      "devicon-react-original",
+      "devicon-postgresql-plain"
     ]
   }
 ]
